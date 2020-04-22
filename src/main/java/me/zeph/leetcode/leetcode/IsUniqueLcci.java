e17eb82a24caeab5d11a428c33fb1e47564cd55c
benweizhu <benwei.zhu@exxonmobil.com>
2020-04-22 16:12:03 +0800

    add IsUniqueLcci

---
+++ src/main/java/me/zeph/leetcode/leetcode/IsUniqueLcci.java
@@ -0,0 +1,17 @@
+package me.zeph.leetcode.leetcode;
+
+import java.util.HashSet;
+
+public class IsUniqueLcci {
+
+  public boolean is(String str) {
+    HashSet<Character> sameChar = new HashSet<>();
+    for (int i = 0; i < str.length(); i++) {
+      if (!sameChar.add(str.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
---
+++ src/test/java/me/zeph/leetcode/leetcode/IsUniqueLcciTest.java
@@ -0,0 +1,20 @@
+package me.zeph.leetcode.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsUniqueLcciTest {
+
+  @Test
+  void should_return_false() {
+    IsUniqueLcci isUniqueLcci = new IsUniqueLcci();
+    assertFalse(isUniqueLcci.is("leetcode"));
+  }
+
+  @Test
+  void should_return_true() {
+    IsUniqueLcci isUniqueLcci = new IsUniqueLcci();
+    assertTrue(isUniqueLcci.is("abc"));
+  }
+}
